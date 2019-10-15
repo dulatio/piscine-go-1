3e82cad485c0b48fe3b3e640f87ba76e811eb593
bazeeko <bazarbayev.rus@gmail.com>
2019-10-16 02:07:21 +0600

    another try yyy

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -4,15 +4,11 @@ import "github.com/01-edu/z01"
 
 func main() {
 
-	var alphabet string = "abcdefghijklmnopqrstuvwxyz"
-	
-	for i:=0; i<=25; i++ {
-
-		z01.PrintRune(rune(alphabet[i]))
-
+	for i:='a'; i<='z'; i++ {
+		
+		z01.PrintRune(i)
 	}
 	
 	z01.PrintRune('\n')
-
 
 }
