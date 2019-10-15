95a45e81c83099916d0e484bc83654d26c1f8618
bazeeko <bazarbayev.rus@gmail.com>
2019-10-15 14:07:25 +0600

    huh

--- myfamily.sh
+++ myfamily.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 curl -s https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json \
-	| jq -r --arg MANID "$HERO_ID" '.[] | select(.id == $MANID) | .connections.relatives'
+	| jq -r --arg MAN_ID "$HERO_ID" '.[] | select(.id == ($MAN_ID|tonumber)) | .connections.relatives'
