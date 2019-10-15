fbceae7e1b703511465fbebc975fc85d61c9ecea
bazeeko <bazarbayev.rus@gmail.com>
2019-10-16 05:43:50 +0600

    cooooomb2

--- printcomb2.go
+++ printcomb2.go
@@ -6,13 +6,13 @@ import "fmt"
 func PrintComb2() {
 	for i := 0; i <= 98; i++ {
 		for j := i + 1; j <= 99; j++ {
-			fmt.Readln(i/10)
-			fmt.Readln(i%10)
+			fmt.Readln(i / 10)
+			fmt.Readln(i % 10)
 			z01.PrintRune(' ')
-			fmt.Readln(j/10)
-			fmt.Readln(j%10)
+			fmt.Readln(j / 10)
+			fmt.Readln(j % 10)
 			z01.PrintRune(',')
-			z01.PrintRune(' ')			
+			z01.PrintRune(' ')
 		}
 	}
 	z01.PrintRune('\n')
