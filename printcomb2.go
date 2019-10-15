29a33f90802870761aa3ec18169c3f441f74258d
bazeeko <bazarbayev.rus@gmail.com>
2019-10-16 05:49:04 +0600

    cooooomb2

--- printcomb2.go
+++ printcomb2.go
@@ -1,16 +1,19 @@
 package piscine
 
 import "github.com/01-edu/z01"
-import "fmt"
 
 func PrintComb2() {
 	for i := 0; i <= 98; i++ {
 		for j := i + 1; j <= 99; j++ {
-			fmt.Readln(i / 10)
-			fmt.Readln(i % 10)
+			var a = i / 10;
+			var b = i % 10;
+			var c = j / 10;
+			var d = j % 10;
+			z01.PrintRune('a')
+			z01.PrintRune('b')
 			z01.PrintRune(' ')
-			fmt.Readln(j / 10)
-			fmt.Readln(j % 10)
+			z01.PrintRune('c')
+			z01.PrintRune('d')
 			z01.PrintRune(',')
 			z01.PrintRune(' ')
 		}
