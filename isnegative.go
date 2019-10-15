b5669c262b1bc916f0c0ab955171cf45d039a044
bazeeko <bazarbayev.rus@gmail.com>
2019-10-16 04:08:24 +0600

    negativeeeeeeeeeeee

--- isnegative.go
+++ isnegative.go
@@ -1,6 +1,6 @@
 package main
 
-import "github.com/01-edu/z01"
+import piscine ".."
 
 func IsNegative(nb int) rune {
 
@@ -13,11 +13,8 @@ func IsNegative(nb int) rune {
 
 func main() {
 
-	z01.PrintRune(IsNegative(1))
-	z01.PrintRune('\n')
-	z01.PrintRune(IsNegative(0))
-	z01.PrintRune('\n')
-	z01.PrintRune(IsNegative(-1))
-	z01.PrintRune('\n')
+	piscine.IsNegative(1)
+	piscine.IsNegative(0)
+	piscine.IsNegative(-1)
 
 }
