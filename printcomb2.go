abeea81126b7e555d01216018e9680457dbcc6e7
bazeeko <bazarbayev.rus@gmail.com>
2019-10-16 05:50:34 +0600

    cooooomb2

--- printcomb2.go
+++ printcomb2.go
@@ -5,10 +5,10 @@ import "github.com/01-edu/z01"
 func PrintComb2() {
 	for i := 0; i <= 98; i++ {
 		for j := i + 1; j <= 99; j++ {
-			var a = i / 10
-			var b = i % 10
-			var c = j / 10
-			var d = j % 10
+			var a int = i / 10
+			var b int = i % 10
+			var c int = j / 10
+			var d int = j % 10
 			z01.PrintRune('a')
 			z01.PrintRune('b')
 			z01.PrintRune(' ')
