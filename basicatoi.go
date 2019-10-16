0e9bad0b947dd581141dd633b984eb136cdc09a0
bazeeko <bazarbayev.rus@gmail.com>
2019-10-17 04:52:03 +0600

    atoooooooooooooi

---
+++ basicatoi.go
@@ -0,0 +1,18 @@
+package piscine
+
+func BasicAtoi(s string) int {
+	str := []rune(s)
+	answer := 0
+	for i := 0; i <= Len(s); i++ {
+		if answer == 0 && str[i] == '0' {
+			answer = 0
+		} else {
+			ten := 1
+			for j := i ; j < Len(s); j++ {
+				ten = ten * 10
+			}
+			answer = answer + (int(str[i]) - 48) * ten
+		}
+	}
+	return answer
+}
