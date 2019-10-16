41abc0dd94e320e6a2534cb9034742571f82bfbb
bazeeko <bazarbayev.rus@gmail.com>
2019-10-17 03:59:57 +0600

    hwg

---
+++ printstr.go
@@ -0,0 +1,11 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintStr(str string) {
+	strc := []rune(str)
+	for _, letter := range strc {
+		z01.PrintRune(letter)
+	}
+	z01.PrintRune('\n')
+}
