8514ad7e96a02c02e41adca7d89289192242da82
bazeeko <bazarbayev.rus@gmail.com>
2019-10-17 19:11:23 +0600

    checkitout

---
+++ sortintegertable.go
@@ -0,0 +1,24 @@
+package piscine
+
+func TabLen(table []int) int {
+	length := 0
+	for _ = range table {
+		length++
+	}
+	return length
+}
+
+func SortIntegerTable(table []int) {
+	change := false
+	for i := 0; i < TabLen(table) - 1; i++ {
+		if table[i] > table[i+1] {
+			temp := table[i]
+			table[i] = table[i+1]
+			table[i+1] = temp
+			change = true
+		}
+	}
+	if change == true {
+		SortIntegerTable(table)
+	}
+}
