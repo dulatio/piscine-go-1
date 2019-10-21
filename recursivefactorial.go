82491da8ad93a7107788e05a8eed647041448674
bazeeko <bazarbayev.rus@gmail.com>
2019-10-21 11:34:55 +0600

    2nd attempt recurfact

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -3,8 +3,8 @@ package piscine
 func RecursiveFactorial(nb int) int {
 	if nb < 0 || nb > 20 {
 		return 0
-	} else if nb == 2 {
-		return 2
+	} else if nb == 1 {
+		return 1
 	}
 	return nb * RecursiveFactorial(nb-1)
 }
