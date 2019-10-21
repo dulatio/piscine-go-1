0593a88b85e2df2ef56d6f461777376ab0a979a6
bazeeko <bazarbayev.rus@gmail.com>
2019-10-21 11:14:09 +0600

    2nd attempt iterfact

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -2,8 +2,11 @@ package piscine
 
 func IterativeFactorial(nb int) int {
 	answer := 1
+	if nb < 0 {
+		return 0
+	}
 	for i := 2; i <= nb; i++ {
-		answer *= nb
+		answer *= i
 	}
 	return answer
 }
