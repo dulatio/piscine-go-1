405122041bc4a3d16294d0137ca9b69d29311c2a
bazeeko <bazarbayev.rus@gmail.com>
2019-10-22 08:54:10 +0600

    ruuuuness i love ruunes

--- nrune.go
+++ nrune.go
@@ -1,6 +1,17 @@
 package piscine
 
+func RuneArrayLength(array []rune) int {
+	length := 0
+	for range table {
+		length++
+	}
+	return length
+}
+
 func NRune(s string, n int) rune {
 	str := []rune(s)
+	if n > RuneArrayLength(str) - 1 {
+		return 0
+	}
 	return str[n-1]
 }
