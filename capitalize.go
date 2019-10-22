8a0e463a449bfd1360ddaf412e871a49bd99c347
bazeeko <bazarbayev.rus@gmail.com>
2019-10-22 11:01:18 +0600

    continuing capitalizing

--- capitalize.go
+++ capitalize.go
@@ -8,6 +8,8 @@ func Capitalize(s string) string {
 	for i := 1; i < RuneArrayLength(str); i++ {
 		if str[i] >= 'a' && str[i] <= 'z' && ((str[i-1] > '9' || str[i-1] < '0') && (str[i-1] > 'z' || str[i-1] < 'a') && (str[i-1] > 'Z' || str[i-1] < 'A')) {
 			str[i] -= 32
+		} else if str[i] >= 'A' && str[i] <= 'Z' && ((str[i-1] <= '9' && str[i-1] >= '0') || (str[i-1] >= 'a' && str[i-1] <= 'z') || (str[i-1] <= 'Z' && str[i-1] >= 'A')) {
+			str[i] += 32
 		}
 	}
 	return string(str)
