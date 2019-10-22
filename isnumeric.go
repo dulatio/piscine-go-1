b7349e8910da21b13881921ec2662672fd548c89
bazeeko <bazarbayev.rus@gmail.com>
2019-10-22 11:09:43 +0600

    numersicas

---
+++ isnumeric.go
@@ -0,0 +1,11 @@
+package piscine
+
+func IsNumeric(s string) bool {
+	str := []rune(s)
+	for i := 0; i < RuneArrayLength(str); i++ {
+		if str[i] > '9' || str[i] < '0') {
+			return false
+		}
+	}
+	return true
+}
